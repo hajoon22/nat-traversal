ce886293125bffb6a28fe772a274bb7da10f906b
hajoon22 <hajoon@hajoon.me>
2026-07-15 23:12:14 +0900

    Add header for ICMP unreachable message handling

---
+++ traversal/icmp/unreach.h
@@ -0,0 +1,23 @@
+#ifndef ICMP_H
+#define ICMP_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+
+#define MAX_DATA_BUFFER 1500
+
+struct icmp_unreach {
+    struct iphdr iph;
+    struct icmphdr icmph;
+
+    uint8_t *data;
+    size_t data_len;
+};
+
+void deinit_icmp_unreach(struct icmp_unreach *rp);
+int read_icmp_unreach(int s, uint32_t orig_dst, struct icmp_unreach *icmpun);
+int send_icmp_unreach(int s, uint32_t saddr, uint16_t sport, uint32_t daddr, uint16_t dport, uint8_t *data, size_t len);
+
+#endif
