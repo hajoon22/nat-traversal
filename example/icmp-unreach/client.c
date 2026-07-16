d117eff2689abd046b3cd85f743f9ef3c60163f3
hajoon22 <hajoon@hajoon.me>
2026-07-16 22:17:14 +0900

    Add ICMP Destination Unreachable client example

---
+++ example/icmp-unreach/client.c
@@ -0,0 +1,37 @@
+/*
+ICMP Destination Unreachable NAT Traversal 
+Client Example
+*/
+#include <stdio.h>
+#include <stdint.h>
+#include <arpa/inet.h>
+
+#include "../../traversal/traversal.h"
+
+int main(void) {
+    struct nt_session nts;
+    nts.stun_addr = ntohl(inet_addr("74.125.250.129"));
+    nts.stun_port = 19302;
+    nts.method = nt_method_icmp_unreach;
+
+    if (init_nt_session(&nts) < 0) {
+        printf("init nt session error\n");
+        return -1;
+    }
+
+    struct nt_send_packet spkt;
+    spkt.daddr = ntohl(inet_addr("1.1.1.1")); // dst
+    spkt.dport = 55266;
+    spkt.data = "hello";
+    spkt.data_len = 5;
+
+    if (nt_send(&nts, &spkt) < 0) {
+        printf("nt send error\n");
+        return -1;
+    }
+    printf("sent\n");
+
+    deinit_nt_session(&nts);
+
+    return 0;
+}
