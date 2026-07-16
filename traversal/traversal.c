9a693978002af3e62b5874d20b86114316658c41
hajoon22 <hajoon@hajoon.me>
2026-07-16 22:16:34 +0900

    Rename nt_icmp_unreach to nt_method_icmp_unreach

--- traversal/traversal.c
+++ traversal/traversal.c
@@ -8,7 +8,7 @@
 
 int init_nt_session(struct nt_session *nts) {
     switch (nts->method) {
-    case nt_icmp_unreach:
+    case nt_method_icmp_unreach:
         return init_nt_icmp_unreach(nts);
     }
 
@@ -26,14 +26,14 @@ void deinit_nt_session(struct nt_session *nts) {
     } 
 
     switch (nts->method) {
-    case nt_icmp_unreach:
+    case nt_method_icmp_unreach:
         return deinit_nt_icmp_unreach(&nts->icmp_unreach);
     }
 }
 
 int nt_read(struct nt_session *nts, struct nt_read_packet *rpkt) {
     switch (nts->method) {
-    case nt_icmp_unreach:
+    case nt_method_icmp_unreach:
         return nt_read_icmp_unreach(nts, rpkt);
     }
 
@@ -42,7 +42,7 @@ int nt_read(struct nt_session *nts, struct nt_read_packet *rpkt) {
 
 int nt_send(struct nt_session *nts, struct nt_send_packet *spkt) {
     switch (nts->method) {
-    case nt_icmp_unreach:
+    case nt_method_icmp_unreach:
         return nt_send_icmp_unreach(nts, spkt);
     }
 
@@ -51,7 +51,7 @@ int nt_send(struct nt_session *nts, struct nt_send_packet *spkt) {
 
 void deinit_nt_read_packet(struct nt_read_packet *rpkt) {
     switch (rpkt->method) {
-    case nt_icmp_unreach:
+    case nt_method_icmp_unreach:
         return deinit_icmp_unreach(&rpkt->icmpun);
     }
 }
