51c508d75769c0968cf2770c13866d1aeda202ee
hajoon22 <hajoon@hajoon.me>
2026-07-20 21:59:02 +0900

    Refactor ICMP handling to use unified context

--- traversal/icmp/nt.c
+++ traversal/icmp/nt.c
@@ -9,6 +9,7 @@
 #include "nt.h"
 
 #include "unreach.h"
+#include "exceeded.h"
 #include "../stun/stun.h"
 
 static int init_keepalive(int s) {
@@ -23,17 +24,17 @@ static int init_keepalive(int s) {
     return pid;
 }
 
-void deinit_nt_icmp_unreach(struct nt_icmp_unreach_context *icmp_unreach) {
-    if (icmp_unreach->socket >= 0) {
-        close(icmp_unreach->socket);
+void deinit_nt_icmp_context(struct nt_icmp_context *ctx) {
+    if (ctx->socket >= 0) {
+        close(ctx->socket);
     }
 }
 
-int init_nt_icmp_unreach(struct nt_session *nts) {
+int init_nt_icmp(struct nt_session *nts) {
     nts->socket = init_stun(
         nts->stun_addr, nts->stun_port, 
-        &nts->icmp_unreach.pub_addr, 
-        &nts->icmp_unreach.mapped_port);
+        &nts->icmp_ctx.pub_addr, 
+        &nts->icmp_ctx.mapped_port);
 
     if (nts->socket < 0) {
         return nts->socket;
@@ -45,39 +46,56 @@ int init_nt_icmp_unreach(struct nt_session *nts) {
         return nts->keepalive_pid;
     }
 
-    nts->icmp_unreach.socket = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-    if (nts->icmp_unreach.socket < 0) {
+    nts->icmp_ctx.socket = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    if (nts->icmp_ctx.socket < 0) {
         deinit_nt_session(nts);
-        return nts->icmp_unreach.socket;
+        return nts->icmp_ctx.socket;
     }
 
-    nts->icmp_unreach.pfd = (struct pollfd){
-        .fd = nts->icmp_unreach.socket,
+    nts->icmp_ctx.pfd = (struct pollfd){
+        .fd = nts->icmp_ctx.socket,
         .events = POLLIN,
     };
 
     return 0;
 }
 
-int nt_read_icmp_unreach(struct nt_session *nts, struct nt_read_packet *rpkt) {
-    int n = poll(&nts->icmp_unreach.pfd, 1, 1000);
+int nt_read_icmp(struct nt_session *nts, struct nt_read_packet *pkt) {
+    int n = poll(&nts->icmp_ctx.pfd, 1, 1000);
     if (n > 0) {
-        int n = read_icmp_unreach(nts->icmp_unreach.socket, nts->stun_addr, &rpkt->icmpun);
-        if (n < 0) {
-            return n;
+        switch (nts->method) {
+            case nt_method_icmp_unreach: {
+                int n = read_icmp_unreach(nts->icmp_ctx.socket, nts->stun_addr, &pkt->icmpun);
+                pkt->method = nts->method;
+                return n;
+            }
+
+            case nt_method_icmp_exceeded: {
+                int n = read_icmp_exceeded(nts->icmp_ctx.socket, nts->stun_addr, &pkt->icmptime);
+                pkt->method = nts->method;
+                return n;
+            }
         }
-        rpkt->method = nts->method;
-        
-        return 0;
     }
 
     return -1;
 }
 
-int nt_send_icmp_unreach(struct nt_session *nts, struct nt_send_packet *spkt) {
-    return send_icmp_unreach(
-        nts->icmp_unreach.socket, 
-        spkt->daddr, spkt->dport, 
-        nts->stun_addr, nts->stun_port, 
-        spkt->data, spkt->data_len);
+int nt_send_icmp(struct nt_session *nts, struct nt_send_packet *pkt) {
+    switch (nts->method) {
+        case nt_method_icmp_unreach:
+            return send_icmp_unreach(
+                nts->icmp_ctx.socket, 
+                pkt->daddr, pkt->dport, 
+                nts->stun_addr, nts->stun_port, 
+                pkt->data, pkt->data_len);
+        case nt_method_icmp_exceeded:
+            return send_icmp_exceeded(
+                nts->icmp_ctx.socket, 
+                pkt->daddr, pkt->dport, 
+                nts->stun_addr, nts->stun_port, 
+                pkt->data, pkt->data_len);        
+    }
+
+    return -1;
 }
