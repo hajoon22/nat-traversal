6ba9ea893cac21d9fc5ac3a110d46a2e883339b0
hajoon22 <hajoon@hajoon.me>
2026-07-20 21:58:34 +0900

    Add handling for ICMP exceeded method in traversal

--- traversal/traversal.c
+++ traversal/traversal.c
@@ -5,11 +5,13 @@
 #include "traversal.h"
 #include "icmp/nt.h"
 #include "icmp/unreach.h"
+#include "icmp/exceeded.h"
 
 int init_nt_session(struct nt_session *nts) {
     switch (nts->method) {
+    case nt_method_icmp_exceeded:
     case nt_method_icmp_unreach:
-        return init_nt_icmp_unreach(nts);
+        return init_nt_icmp(nts);
     }
 
     return -1;
@@ -27,14 +29,16 @@ void deinit_nt_session(struct nt_session *nts) {
 
     switch (nts->method) {
     case nt_method_icmp_unreach:
-        return deinit_nt_icmp_unreach(&nts->icmp_unreach);
+    case nt_method_icmp_exceeded:
+        return deinit_nt_icmp_context(&nts->icmp_ctx);
     }
 }
 
 int nt_read(struct nt_session *nts, struct nt_read_packet *rpkt) {
     switch (nts->method) {
+    case nt_method_icmp_exceeded:
     case nt_method_icmp_unreach:
-        return nt_read_icmp_unreach(nts, rpkt);
+        return nt_read_icmp(nts, rpkt);
     }
 
     return -1;
@@ -42,8 +46,9 @@ int nt_read(struct nt_session *nts, struct nt_read_packet *rpkt) {
 
 int nt_send(struct nt_session *nts, struct nt_send_packet *spkt) {
     switch (nts->method) {
+    case nt_method_icmp_exceeded:
     case nt_method_icmp_unreach:
-        return nt_send_icmp_unreach(nts, spkt);
+        return nt_send_icmp(nts, spkt);
     }
 
     return -1;
@@ -53,5 +58,7 @@ void deinit_nt_read_packet(struct nt_read_packet *rpkt) {
     switch (rpkt->method) {
     case nt_method_icmp_unreach:
         return deinit_icmp_unreach(&rpkt->icmpun);
+    case nt_method_icmp_exceeded:
+        return deinit_icmp_exceeded(&rpkt->icmptime);
     }
 }
