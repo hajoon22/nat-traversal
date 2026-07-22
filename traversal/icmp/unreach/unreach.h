5434333fbbaebc2d3225a3739ffa38db008940de
hajoon22 <hajoon@hajoon.me>
2026-07-22 20:58:07 +0900

    Add ICMP unreachable header file

---
+++ traversal/icmp/unreach/unreach.h
@@ -0,0 +1,28 @@
+#ifndef ICMP_UNREACH_H
+#define ICMP_UNREACH_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+
+#define MAX_DATA_BUFFER 1500
+
+struct nt_session;
+struct nt_read_packet;
+struct nt_send_packet;
+
+struct icmp_unreach {
+    struct iphdr iph;
+    struct icmphdr icmph;
+
+    uint8_t *data;
+    size_t data_len;
+};
+
+void deinit_icmp_unreach(struct icmp_unreach *icmpunreach);
+
+int send_icmp_unreach(struct nt_session *nts, struct nt_send_packet *pkt);
+int read_icmp_unreach(struct nt_session *nts, struct nt_read_packet *pkt);
+
+#endif
