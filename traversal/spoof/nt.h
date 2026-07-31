b65d0f87aa7eb8c747687ebf7fc890709f98d030
hajoon22 <hajoon@hajoon.me>
2026-08-01 01:23:56 +0900

    Add echo parameters to nt_spoof_context structure

--- traversal/spoof/nt.h
+++ traversal/spoof/nt.h
@@ -8,14 +8,25 @@ struct nt_session;
 struct nt_read_packet;
 struct nt_send_packet;
 
+#define ECHO_ID 1111
+#define ECHO_SEQ 2222
+#define ECHO_ADDR "1.1.1.1"
+
 struct nt_spoof_context {
-    int socket;
+    int send_socket;
+    int read_socket;
+    
     struct pollfd pfd;
 
     uint32_t pub_addr;
     uint16_t mapped_port;
 
     uint32_t relay_addr;
+
+    // icmp echo
+    uint16_t id;
+    uint16_t seq;
+    uint32_t addr;
 };
 
 int init_nt_spoof(struct nt_session *nts);
