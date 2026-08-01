d73de56939e733b1b21cd3de0f5df4e103b42e82
hajoon22 <hajoon@hajoon.me>
2026-08-02 00:28:48 +0900

    Remove icmp_exceeded structure and deinit function
    Removed the definition of the icmp_exceeded structure and its deinitialization function.

--- traversal/icmp/exceeded/exceeded.h
+++ traversal/icmp/exceeded/exceeded.h
@@ -6,21 +6,9 @@
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
 
+#include "../../traversal.h"
+
 #define MAX_DATA_BUFFER 1500
-
-struct nt_session;
-struct nt_read_packet;
-struct nt_send_packet;
-
-struct icmp_exceeded {
-    struct iphdr iph;
-    struct icmphdr icmph;
-
-    uint8_t *data;
-    size_t data_len;
-};
-
-void deinit_icmp_exceeded(struct icmp_exceeded *icmptime);
 
 int send_icmp_exceeded(struct nt_session *nts, struct nt_send_packet *pkt);
 int read_icmp_exceeded(struct nt_session *nts, struct nt_read_packet *pkt);
