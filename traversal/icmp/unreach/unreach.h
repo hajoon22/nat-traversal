30105c8f2c6bc727646966bd239601407f141fbb
hajoon22 <hajoon@hajoon.me>
2026-08-02 00:28:26 +0900

    Remove icmp_unreach structure and deinit function
    Removed the icmp_unreach structure and its deinitialization function.

--- traversal/icmp/unreach/unreach.h
+++ traversal/icmp/unreach/unreach.h
@@ -6,20 +6,9 @@
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
 
+#include "../../traversal.h"
+
 #define MAX_DATA_BUFFER 1500
-
-struct nt_session;
-struct nt_read_packet;
-struct nt_send_packet;
-
-struct icmp_unreach {
-    struct iphdr iph;
-
-    uint8_t *data;
-    size_t data_len;
-};
-
-void deinit_icmp_unreach(struct icmp_unreach *icmpunreach);
 
 int send_icmp_unreach(struct nt_session *nts, struct nt_send_packet *pkt);
 int read_icmp_unreach(struct nt_session *nts, struct nt_read_packet *pkt);
