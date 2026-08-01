4f68e74ec04389351aef5c0647fd56b9d7cac314
hajoon22 <hajoon@hajoon.me>
2026-08-02 00:27:40 +0900

    Clean up echo.h by removing unused code
    Removed unused structures and function declaration from echo.h

--- traversal/spoof/echo/echo.h
+++ traversal/spoof/echo/echo.h
@@ -5,18 +5,9 @@
 #include <stddef.h>
 #include <netinet/ip.h>
 
+#include "../../traversal.h"
+
 #define MAX_DATA_BUFFER 1500
-
-struct nt_session;
-struct nt_read_packet;
-struct nt_send_packet;
-
-struct spoof_echo {
-    uint8_t *data;
-    size_t data_len;
-};
-
-void deinit_spoof_echo(struct spoof_echo *spoofecho);
 
 int send_spoof_echo(struct nt_session *nts, struct nt_send_packet *pkt);
 int read_spoof_echo(struct nt_session *nts, struct nt_read_packet *pkt);
