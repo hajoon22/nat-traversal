0e8e2b93bedb3463a19f012158b3c08aa7bae6cd
hajoon22 <hajoon@hajoon.me>
2026-08-03 00:52:24 +0900

    Add ICMP Time Exceeded NAT Traversal server example
    This file implements a server example for ICMP Time Exceeded NAT traversal, initializing a session and reading packets.

---
+++ example/icmp-exceeded/icmp/server.c
@@ -0,0 +1,35 @@
+/*
+ICMP Time Exceeded (ICMP) NAT Traversal 
+Server Example
+*/
+#include <stdio.h>
+#include <stdint.h>
+#include <arpa/inet.h>
+
+#include "../../../traversal/traversal.h"
+
+int main(void) {
+    struct nt_session nts;
+    nts.method = nt_method_icmp_exceeded_icmp;
+
+    if (init_nt_session(&nts) < 0) {
+        printf("init nt session error\n");
+        return -1;
+    }
+    
+    struct nt_read_packet rpkt;
+    while (1) {
+        if (nt_read(&nts, &rpkt) < 0) {
+            continue;
+        }
+
+        printf("recived data: %.*s\n", (int)rpkt.data_len, (char *)rpkt.data);
+        deinit_nt_read_packet(&rpkt);
+
+        break;
+    }
+
+    deinit_nt_session(&nts);
+
+    return 0;
+}
