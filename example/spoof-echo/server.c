0c4d6990619178ed3ecaf5d96d3b1494ab37dca9
hajoon22 <hajoon@hajoon.me>
2026-08-03 01:06:18 +0900

    Add spoof-echo server example in C
    Implement a UDP unreachable NAT traversal server example.

---
+++ example/spoof-echo/server.c
@@ -0,0 +1,43 @@
+/*
+SPOOF UDP Unreachable NAT Traversal 
+Server Example
+*/
+#include <stdio.h>
+#include <stdint.h>
+#include <arpa/inet.h>
+
+#include "../../traversal/traversal.h"
+
+int main(void) {
+    struct nt_session nts;
+    nts.stun_addr = ntohl(inet_addr("74.125.250.129"));
+    nts.stun_port = 19302;
+    nts.method = nt_method_spoof_echo_reflection;
+    
+    uint32_t relay_addr = ntohl(inet_addr("127.0.0.1"));
+
+    if (init_nt_session(&nts, relay_addr) < 0) {
+        printf("init nt session error\n");
+        return -1;
+    }
+
+    struct in_addr a;
+    a.s_addr = htonl(nts.spoof_ctx->pub_addr);
+    printf("public ip = %s\n", inet_ntoa(a));
+
+    struct nt_read_packet rpkt;
+    while (1) {
+        if (nt_read(&nts, &rpkt) < 0) {
+            continue;
+        }
+
+        printf("recived data: %.*s\n", (int)rpkt.data_len, (char *)rpkt.data);
+        deinit_nt_read_packet(&rpkt);
+
+        break;
+    }
+
+    deinit_nt_session(&nts);
+
+    return 0;
+}
