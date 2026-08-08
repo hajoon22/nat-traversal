6fbb56025905eaca0787e0999d09035974e75d27
hajoon22 <hajoon@hajoon.me>
2026-08-08 22:41:40 +0900

    Add ISTUN request and reply parsing functions

--- traversal/istun/istun.c
+++ traversal/istun/istun.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <unistd.h>
+#include <poll.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
@@ -59,4 +60,68 @@ int init_istun() {
     }
 
     return listen_istun(s);
+}
+
+static int parse_istun_reply(uint32_t istun_addr, uint8_t *buf, size_t len) {
+    if (len < sizeof(struct iphdr)+sizeof(struct icmphdr)+sizeof(uint16_t)) {
+        return -1;
+    }
+
+    size_t offset = 0;
+    struct iphdr *iph = (struct iphdr *)buf;
+    offset += sizeof(struct iphdr);
+
+    if (ntohl(iph->saddr) != istun_addr) {
+        return -1;
+    }
+
+    struct icmphdr *icmph = (struct icmphdr *)(buf+offset);
+    offset += sizeof(struct icmphdr);
+
+    if (icmph->type != ICMP_ECHOREPLY) {
+        return -1;
+    }
+
+    uint16_t mapped_id;
+    memcpy(&mapped_id, buf+offset, sizeof(mapped_id));
+
+    mapped_id = ntohs(mapped_id);
+
+    return (int)mapped_id;
+}
+
+int send_istun_request(uint32_t istun_addr, uint16_t sid) {
+    int s = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    if (s < 0) {
+        return s;
+    }
+
+    struct icmphdr icmph = {0};
+    icmph.type = ICMP_ECHO;
+    icmph.un.echo.id = htons(sid);
+    icmph.un.echo.sequence = htons(2222);    
+    icmph.checksum = htons(checksum((uint8_t *)&icmph, sizeof(icmph)));
+
+    struct sockaddr_in sin;
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = htonl(istun_addr);
+
+    sendto(s, &icmph, sizeof(icmph), 0, (struct sockaddr *)&sin, sizeof(sin));
+
+    struct pollfd pfd = {
+        .fd = s,
+        .events = POLLIN
+    };
+
+    // timeout: 10 sec
+    int r = poll(&pfd, 1, 10000);
+    if (r > 0) {
+        uint8_t buf[MAX_DATA_BUFFER];
+        int n = read(s, buf, MAX_DATA_BUFFER);
+
+        int ret = parse_istun_reply(istun_addr, buf, n);
+        return ret;
+    }
+
+    return -1; // timeout
 }
