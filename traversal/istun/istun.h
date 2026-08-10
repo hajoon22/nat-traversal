1860f0cd1cf951ad1db3c7280c59c8d794ca9832
hajoon22 <hajoon@hajoon.me>
2026-08-10 23:31:13 +0900

    Add ISTUN request and reply constants

--- traversal/istun/istun.h
+++ traversal/istun/istun.h
@@ -4,6 +4,9 @@
 #define MAX_DATA_BUFFER 1500
 #define REPLY_SIZE sizeof(struct icmphdr)+sizeof(uint16_t)
 
+#define ISTUN_REQUEST 2010
+#define ISTUN_REPLY 0222
+
 int init_istun();
 int send_istun_request(uint32_t istun_addr, uint16_t sid);
 
