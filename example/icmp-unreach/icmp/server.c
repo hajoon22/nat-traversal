ef790490fbc235755508eaa5cd9dcfdc22fa05fb
hajoon22 <hajoon@hajoon.me>
2026-08-10 23:20:58 +0900

    Set istun_addr to zero in ICMP server
    Initialize istun_addr to zero for ICMP session.

--- example/icmp-unreach/icmp/server.c
+++ example/icmp-unreach/icmp/server.c
@@ -1,6 +1,8 @@
 /*
 ICMP Destination Unreachable (ICMP) NAT Traversal 
 Server Example
+
+Ubuntu (kernel 6.8.0-71-generic, x86_64) Symmetric NAT (MASQUERADE --random-fully)
 */
 #include <stdio.h>
 #include <stdint.h>
@@ -11,12 +13,15 @@ Server Example
 int main(void) {
     struct nt_session nts;
     nts.method = nt_method_icmp_unreach_icmp;
-
+    nts.istun_addr = ntohl(inet_addr(""));
+    
     if (init_nt_session(&nts) < 0) {
         printf("init nt session error\n");
         return -1;
     }
     
+    printf("mapped id = %d\n", nts.mapped_id);
+
     struct nt_read_packet rpkt;
     while (1) {
         if (nt_read(&nts, &rpkt) < 0) {
