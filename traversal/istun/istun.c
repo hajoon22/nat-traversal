e5b4c82f068f4fe9198238cd0d81f7d27fd04638
hajoon22 <hajoon@hajoon.me>
2026-08-12 01:34:25 +0900

    Update includes in istun.c for common headers

--- traversal/istun/istun.c
+++ traversal/istun/istun.c
@@ -11,7 +11,9 @@
 #include <netinet/ip_icmp.h>
 
 #include "istun.h"
-#include "../checksum/checksum.h"
+
+#include "../common/common.h"
+#include "../common/checksum.h"
 
 static void build_istun_reply(uint8_t buf[REPLY_SIZE], uint16_t id) {
         struct icmphdr *icmph = (struct icmphdr *)buf;
