ca34a8a327736ad49315ac65a3faa37486f0c9d8
hajoon22 <hajoon@hajoon.me>
2026-08-12 01:34:14 +0900

    Update ISTUN_REPLY definition and remove MAX_DATA_BUFFER

--- traversal/istun/istun.h
+++ traversal/istun/istun.h
@@ -1,11 +1,10 @@
 #ifndef ISTUN_H
 #define ISTUN_H
 
-#define MAX_DATA_BUFFER 1500
 #define REPLY_SIZE sizeof(struct icmphdr)+sizeof(uint16_t)
 
 #define ISTUN_REQUEST 2010
-#define ISTUN_REPLY 0222
+#define ISTUN_REPLY 2222
 
 int init_istun();
 int send_istun_request(uint32_t istun_addr, uint16_t sid);
