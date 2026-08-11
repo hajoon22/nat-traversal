a4f031ce6e55d425463e9df072dc01bc55bba0ba
hajoon22 <hajoon@hajoon.me>
2026-08-12 01:32:52 +0900

    Refactor nt.c by removing keepalive functions
    Removed unused keepalive functions and updated includes.

--- traversal/spoof/nt.c
+++ traversal/spoof/nt.c
@@ -14,41 +14,9 @@
 #include "../traversal.h"
 #include "../stun/stun.h"
 #include "../istun/istun.h"
-#include "../checksum/checksum.h"
-
-static int init_keepalive_udp(int s) {
-    int pid = fork();
-    if (pid == 0) {
-        while (1) {
-            send(s, "hello", 5, 0); // keepalive
-            sleep(10);
-        }
-    }
-
-    return pid;
-}
-
-static int init_keepalive_icmp(int s, uint32_t istun_addr) {
-    int pid = fork();
-    if (pid == 0) {
-        struct icmphdr icmph = {0};
-        icmph.type = ICMP_ECHO;
-        icmph.un.echo.id = htons(ECHO_ID);
-        icmph.un.echo.sequence = htons(ECHO_SEQ);    
-        icmph.checksum = htons(checksum((uint8_t *)&icmph, sizeof(icmph)));
-
-        struct sockaddr_in sin;
-        sin.sin_family = AF_INET;
-        sin.sin_addr.s_addr = htonl(istun_addr);
-
-        while (1) {
-            sendto(s, &icmph, sizeof(icmph), 0, (struct sockaddr *)&sin, sizeof(sin));
-            sleep(5);
-        }
-    }
-
-    return pid;
-}
+#include "../common/common.h"
+#include "../common/checksum.h"
+#include "../common/keepalive.h"
 
 void deinit_nt_spoof_context(struct nt_spoof_context *ctx) {
     close(ctx->read_socket);
