9c00c99b139b7fbbc067ca3de24ceab36d7df3f9
hajoon22 <hajoon@hajoon.me>
2026-08-12 01:33:45 +0900

    Remove MAX_DATA_BUFFER definition
    Removed the MAX_DATA_BUFFER definition from echo.h.

--- traversal/spoof/echo/echo.h
+++ traversal/spoof/echo/echo.h
@@ -7,8 +7,6 @@
 
 #include "../../traversal.h"
 
-#define MAX_DATA_BUFFER 1500
-
 int send_spoof_echo(struct nt_session *nts, struct nt_send_packet *pkt);
 int read_spoof_echo(struct nt_session *nts, struct nt_read_packet *pkt);
 
