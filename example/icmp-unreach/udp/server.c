bca55b491acee5abfd55c3a5b3c1ac31b27ae242
hajoon22 <hajoon@hajoon.me>
2026-08-20 20:47:17 +0900

    Fix typo in received data log message

--- example/icmp-unreach/udp/server.c
+++ example/icmp-unreach/udp/server.c
@@ -32,7 +32,7 @@ int main(void) {
             continue;
         }
 
-        printf("recived data: %.*s\n", (int)rpkt.data_len, (char *)rpkt.data);
+        printf("received data: %.*s\n", (int)rpkt.data_len, (char *)rpkt.data);
         deinit_nt_read_packet(&rpkt);
 
         break;
